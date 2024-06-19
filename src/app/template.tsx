6a9907672f90207e4cf6d285e9e6815f7e13d4cd
Prince Dwivedi <Prince.Dwivedi@jacobs.com>
2024-06-19 09:46:22 +0530

    fixed redirect issue

--- src/app/template.tsx
+++ src/app/template.tsx
@@ -3,11 +3,19 @@ import Navbar from "@/components/Navbar";
 import Header from "@/components/header";
 import { AppProps } from "next/app";
 import { usePathname } from "next/navigation";
+import { useEffect } from "react";
 import { useSelector } from "react-redux";
+import { useRouter } from "next/navigation";
 
 export default function Template({ children }: { children: React.ReactNode }) {
   const path = usePathname();
+  const router = useRouter();
   const { user } = useSelector((state: any) => state.user);
+  useEffect(() => {
+    if (!path.includes("/login") && !user.islogin) {
+      router.push("/login");
+    }
+  });
   // console.log(user, "SDdsd");
   return path.includes("/login") ? (
     <div>{children}</div>
